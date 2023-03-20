abea1d666d44472f912e2b31c45c9c1692d311d3
RenJeka <renjeka@gmail.com>
2023-03-20 12:12:16 +0200

    initial for github

---
+++ server-2.js
@@ -0,0 +1,93 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const port = 8080;
+
+const data = [
+    {name: 'dog', age: 3},
+    {name: 'cat', age: 2},
+    {name: 'human', age: 23},
+]
+
+const indexPath = './index.html';
+
+http.createServer((req, res) => {
+
+    switch (req.method) {
+        case 'GET': {
+            sendPageByRoute(req, res);
+            break;
+        }
+
+        case 'POST': {
+            modifyData(req, res);
+            break;
+        }
+
+        default: {
+
+        }
+    }
+}).listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+
+/**
+ *
+ * @param request httpRequest
+ * @param response httpResponse
+ */
+function sendPageByRoute(request, response) {
+    const route = url.parse(request.url, true).path;
+    console.log(route);
+
+    switch (route) {
+        case '/':
+        case '/home':
+        case '/index':
+        case '/index.html': {
+            fs.readFile(indexPath, (error, data) => {
+                if (error) throw error;
+                response.setHeader('Content-Type', 'text/html');
+                response.end(data.toString());
+            });
+            return;
+        }
+        case '/data': {
+            response.setHeader('Content-Type', 'application/json');
+            response.end(JSON.stringify(data));
+            return ;
+        }
+        default: {
+            return '<h2 style="color: darkred">Oops! Page not found!</h2>';
+        }
+    }
+}
+
+function modifyData(request, response) {
+    let body = [];
+    request.on('data', (chunk) => {
+        console.log('chunk: ');
+        console.log(JSON.parse(chunk));
+        body.push(JSON.parse(chunk));
+
+    }).on('end', () => {
+        // data.push(bodyParser(body));
+        data.push(...body);
+        // console.log('end: ', bodyParser(body));
+        
+        sendPageByRoute(request, response);
+    });
+
+}
+
+function bodyParser(body) {
+    const entries = [];
+    const splicedBodyArr = body.split('&');
+
+    splicedBodyArr.forEach((property) => {
+        const entry = property.split('=');
+        entries.push(entry)
+    });
+    return  Object.fromEntries(entries)
+}
